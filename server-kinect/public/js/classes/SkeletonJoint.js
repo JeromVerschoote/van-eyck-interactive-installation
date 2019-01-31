1910a85a2f31abc80146183e8b90403e56c96b3c
JeromVerschoote <33222896+JeromVerschoote@users.noreply.github.com>
2019-01-31 01:15:22 +0100

    optimized code

---
+++ server-kinect/public/js/classes/SkeletonJoint.js
@@ -0,0 +1,67 @@
+const GESTURE_SENSITIVITY = 0.34;
+
+class SkeletonJoint{
+    constructor(){
+        this.joint;
+        this.position = {};
+        this.skeletonCenter;
+    }
+
+    /* METHODS */
+
+    update(joint, skeletonCenter){
+        this.joint = joint;
+        //
+        this.position.x = this.joint.depthX;
+        this.position.y = this.joint.depthY;
+
+        if(skeletonCenter){
+            this.skeletonCenter = skeletonCenter;
+        }
+    }
+
+    addSwipeLeftListener(skeletonPart){
+        if(this._getRelativeSpeed().x < -GESTURE_SENSITIVITY){
+            const gesture = `swipeLeft`;
+            console.log(`Gesture detected: ${gesture} (${skeletonPart})`);
+
+            this.tempRelativeSpeed = this.relativePosition;
+        }
+    }
+
+    addSwipeRightListener(skeletonPart){
+        if(this._getRelativeSpeed().x > GESTURE_SENSITIVITY){
+            const gesture = `swipeRight`;
+            console.log(`Gesture detected: ${gesture} (${skeletonPart})`);
+
+            this.tempRelativeSpeed = this.relativePosition;
+        }
+    }
+
+    /* PRIVATE FUNCTIONS  */
+
+    _getRelativePosition(){
+        let relativePosition = {};
+
+        relativePosition.x = this.position.x - skeletonCenter.position.x;
+        relativePosition.y = this.position.y - skeletonCenter.position.y;
+
+        return relativePosition;
+    }
+
+    _getRelativeSpeed(){
+            if(this.tempRelativeSpeed){
+                this.relativePosition = this._getRelativePosition();
+
+                this.relativeSpeed = {
+                    'x': this.relativePosition.x - this.tempRelativeSpeed.x,
+                    'y': this.relativePosition.y - this.tempRelativeSpeed.y,
+                }
+
+                return this.relativeSpeed;
+            }else{
+                this.relativePosition = this._getRelativePosition();
+                this.tempRelativeSpeed = this.relativePosition;
+            }
+    }
+}
--- server-kinect/public/js/script.js
+++ server-kinect/public/js/script.js
@@ -1,49 +1,46 @@
 let socket;
 
-let rightHandPosition, torsoPosition;
-let rightHandRelativePosition, rightHandRelativeSpeed, tempSpeed;
+let skeletonCenter;
+let skeleton = {}
 
 const init = () => {
     socket = io.connect(`/`);
+    
+    skeletonCenter = new SkeletonJoint();
+    skeleton.handRight = new SkeletonJoint();
+    skeleton.handLeft = new SkeletonJoint();
+
     socket.on(`connect`, () => {
         console.log(`Connected: ${socket.id}`);
     });
 
     socket.on('bodyFrame', frame => {
-
-        frame.bodies.forEach(body => {
-            if(body.tracked){
-               
-                rightHandPosition = body.joints[11].depthX;
-                torsoPosition = body.joints[1].depthX;
-
-                setInterval(() => {
-                    if(tempSpeed){
-                        rightHandRelativePosition = rightHandPosition - torsoPosition;
-                        rightHandRelativeSpeed = rightHandRelativePosition - tempSpeed;
-
-                        if(rightHandRelativeSpeed < -0.34){
-                            const gesture = `swipeLeft`;
-                            console.log(`Gesture detected: ${gesture}`);
-                            socket.emit(`gesture`, gesture);
-                            tempSpeed = rightHandRelativePosition;
-                        }
-
-                        if(rightHandRelativeSpeed > 0.34){
-                            const gesture = `swipeRight`;
-                            console.log(`Gesture detected: ${gesture}`);
-                            socket.emit(`gesture`, gesture);
-                            tempSpeed = rightHandRelativePosition;
-                        }
-                    }else{
-                        rightHandRelativePosition = rightHandPosition - torsoPosition;
-                        tempSpeed = rightHandRelativePosition;
-                    }
-                }, 1000);
-
-            }
-        });
+        updateFrame(frame);
     });
 };
+
+const updateFrame = frame => {
+    frame.bodies.forEach(body => {
+
+        if(body.tracked){
+            skeletonCenter.update(body.joints[1]);
+            skeleton.handRight.update(body.joints[11], skeletonCenter);
+            skeleton.handLeft.update(body.joints[7], skeletonCenter);
+
+            addGestureListeners();
+        };
+    });
+}
+
+const addGestureListeners = () => {
+    const INTERVAL_RATE = 1000;
+
+    setInterval(() => {
+        Object.keys(skeleton).forEach(key => {
+            skeleton[key].addSwipeLeftListener(key);
+            skeleton[key].addSwipeRightListener(key);
+        });
+    }, INTERVAL_RATE);
+}
 
 init();
