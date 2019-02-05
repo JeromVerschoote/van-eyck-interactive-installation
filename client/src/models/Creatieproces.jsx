cd08a33e035a1b1cf9205d707d1affc321838b86
justineVanderplancke <justine.vanderplancke@student.howest.be>
2019-02-05 10:39:36 +0100

    HTML added (creatieproces & levensloop)

--- client/src/models/Creatieproces.jsx
+++ client/src/models/Creatieproces.jsx
@@ -7,31 +7,28 @@ const Creatieproces = () => {
       <div className='book' id='book'>
       <canvas className='pageflip-canvas' id="pageflip-canvas"></canvas>
       <div className='pages' id="pages">
-        <section> 
+      <section className='section'> 
           <header>
           <img src={Logo} alt="Dit is het logo van MSK Gent." width="400" height="400" />
           <h1 className="_h1 --color-blue">van eyck: een optische revolutie</h1>
-          <h2>Creatieproces</h2>
-          <div>taalkeuze</div>
+          <h2>creatieproces</h2>
+          <div><hr/><button>nl</button><button>fr</button><button>de</button><button>en</button><button>it</button><button>es</button><hr/></div>
           </header>
           <article>
-
+           <p><span>I</span>n de 15de eeuw was olieverf een belangrijk medium in schilderkunst. Jan Van Eyck was één schilders die de Vlaamse Primitieven vertegenwoordigde en die olieverf kunst op de kaart heeft gezet. Hij gebruikte nooit eerder vertoonde technieken waardoor er nieuwe demensies naar voor kwamen en beïnvloede hiermee tijdgenoten.</p>
           </article>
         </section>
-        <section>
-          
-        </section>
-        <section>
-          
-        </section>
-        <section>
-          
-        </section>
-        <section>
-          
-        </section>
-        <section>
-          
+        <section className='section right'>
+          <div>
+            <hr/><a href="#">RESTART... OTHER NAME</a><hr/>
+          </div>
+          <article>
+            <header>
+               <h3><span>O</span>lieverf kunst</h3>
+                <p>Olieverf kunst bestaat uit een heel proces die zorgvuldig moet worden doorlopen.</p>
+            </header>
+            <p>De kunstenaar moet geduldig verscheidene stappen doornemen die het schilderij vormgeven. Elke stap is een belangrijke factor om tot het eindresultaat te komen. Neem een kijkje achter het grootste werk van de gebroeder Van Eyck: het Lam Gods.</p>
+          </article>
         </section>
       </div>
     </div>
