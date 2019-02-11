b1062f4693b753c5b1a861e834ce39d330412ace
Jerom Verschoote <jeromverschoote@gmail.com>
2019-02-11 12:42:32 +0100

    optimized code

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -12,119 +12,94 @@ class Canvas extends Component {
 
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    let offsetNavigation = 0;
+
+
 
     if(parent === 'leefwereld'){
       
+
       details.forEach(detail => {
         const {title, text, img, position, dimensions, options} = detail;
-
-        let lines;
-
-        if(detail === details[0]){
-          lines = wrapTextFromCanvasIntoLines(ctx, text, dimensions.width - (PADDING * 5.5));
-        }else{
-          lines = wrapTextFromCanvasIntoLines(ctx, text, dimensions.width - (PADDING * 2));
-        }
-        
-        let offset = 0;
-
         const image = document.getElementById(img.src);
+        let lines, offset = 0;
 
-        switch(options.pointedTo){
-          case 'lowerLeft':
-              this.drawBox(ctx, position.x, position.y, dimensions.width, -dimensions.height, COLOR.white);
-              this.drawText(ctx, title, position.x + PADDING, position.y - dimensions.height + (PADDING * 1.5), COLOR.black, FONT.bold, '18px', 0);
-              lines.forEach(line => {
-                this.drawText(ctx, line, position.x + PADDING,position.y - dimensions.height + (PADDING * 2.5), COLOR.black, FONT.regular, '14px', offset);
-                offset += 25;
-              });
-              this.drawImage(ctx, image, position.x, position.y - img.height, img.width, img.height);
-              this.drawPointer(ctx, position.x, position.y -35, position.x, position.y, position.x + 35, position.y, COLOR.red);
-          break;
+        detail === details[0] ? lines = wrapTextFromCanvasIntoLines(ctx, text, dimensions.width - (PADDING * 5.5)) : lines = wrapTextFromCanvasIntoLines(ctx, text, dimensions.width - (PADDING * 2));
 
-          case 'topLeft':
-            this.drawBox(ctx, position.x, position.y, dimensions.width, dimensions.height, COLOR.white);
-            this.drawText(ctx, title, position.x + PADDING, position.y+ (PADDING * 1.5) + img.height, COLOR.black, FONT.bold, '18px', 0);
-            lines.forEach(line => {
-              this.drawText(ctx, line, position.x + PADDING,position.y + (PADDING * 2.5) + img.height, COLOR.black, FONT.regular, '14px', offset);
-              offset += 25;
-            });
-            this.drawImage(ctx, image, position.x, position.y, img.width, img.height);
-            this.drawPointer(ctx, position.x, position.y, position.x, position.y +35, position.x +35, position.y, COLOR.red);
-          break;
-
-          case 'topRight':
-           this.drawBox(ctx, position.x, position.y, -dimensions.width, dimensions.height, COLOR.white);
-           this.drawText(ctx, title, position.x - dimensions.width + PADDING, position.y + (PADDING * 1.5) + img.height, COLOR.black, FONT.bold, '18px', 0);
-           lines.forEach(line => {
-            this.drawText(ctx, line, position.x - dimensions.width + PADDING,position.y + (PADDING * 2.5) + img.height, COLOR.black, FONT.regular, '14px', offset);
-            offset += 25;
-          });
-          this.drawImage(ctx, image, position.x - img.width, position.y, img.width, img.height);
-          this.drawPointer(ctx, position.x, position.y, position.x -35, position.y, position.x, position.y +35, COLOR.red);
-          break;
-
-          case 'lowerRight':
-            this.drawBox(ctx, position.x, position.y, -dimensions.width, -dimensions.height, COLOR.white);
-            this.drawText(ctx, title, position.x - dimensions.width + PADDING, position.y - dimensions.height + (PADDING * 1.5), COLOR.black, FONT.bold, '18px', 0);
-            lines.forEach(line => {
-              this.drawText(ctx, line, position.x - dimensions.width + PADDING, position.y - dimensions.height + (PADDING * 2.5), COLOR.black, FONT.regular, '14px', offset);
-              offset += 25;
-            });
-            this.drawImage(ctx, image, position.x -  img.width, position.y - img.height, img.width, img.height);
-            this.drawPointer(ctx, position.x, position.y, position.x -35, position.y, position.x, position.y -35, COLOR.red);
-          break;
-
-          default:
-          console.log('No direction provided in json data.')
-          break;
-        }
+        this.drawCard(ctx, title, lines, offset, img, image, position, dimensions, options);
       });
+
+
     }else if(parent === 'creatieproces'){
-      //navigatie weergeven
-      details.forEach(detail => {
-        this.drawText(ctx, detail.title, 1300 + offsetNavigation, 600 + (PADDING * 1.5), COLOR.white, FONT.bold, '20px', 0);
-        offsetNavigation += 150;
-      })
 
-      //navigatie doorgeven in array
-      const laag = details[0];
-      const navigatieCreatieproces = laag.details;
 
-      const creatieprocesExplanationLines = wrapTextFromCanvasIntoLines(ctx, laag.explanation, 600 - (PADDING * 2));
-      const creatieprocesLines = wrapTextFromCanvasIntoLines(ctx, navigatieCreatieproces.text, navigatieCreatieproces.dimensions.width - (PADDING * 2));
-      let offset = 0;
+      const currentLayer = details[0];
 
-      creatieprocesExplanationLines.forEach(line => {
-              this.drawText(ctx, line, 1200, 700 + (PADDING * 2.5), COLOR.white, FONT.regular, '18px', offset);
-              offset += 25;
-      });
+      if(currentLayer){
+        const {title, text, img, position, dimensions, options} = currentLayer;
+        
+        const image = document.getElementById(img.src);
+        let lines, offset = 0;
 
-      switch(navigatieCreatieproces.options.pointedTo){
-        case 'topLeft':
-            this.drawBox(ctx, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y, navigatieCreatieproces.dimensions.width, navigatieCreatieproces.dimensions.height, COLOR.white);
-            this.drawText(ctx, navigatieCreatieproces.subtitle, navigatieCreatieproces.position.x + PADDING, navigatieCreatieproces.position.y+ (PADDING * 1.5) + navigatieCreatieproces.detailimg.height, COLOR.black, FONT.bold, '18px', 0);
-            creatieprocesLines.forEach(line => {
-              this.drawText(ctx, line, navigatieCreatieproces.position.x + PADDING,navigatieCreatieproces.position.y + (PADDING * 2.5) + navigatieCreatieproces.detailimg.height, COLOR.black, FONT.regular, '14px', offset);
-              offset += 25;
-            });
-            this.drawPointer(ctx, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y +35, navigatieCreatieproces.position.x +35, navigatieCreatieproces.position.y, COLOR.red);
-        break;
-        case 'topRight':
-           this.drawBox(ctx, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y, -navigatieCreatieproces.dimensions.width, navigatieCreatieproces.dimensions.height, COLOR.white);
-           this.drawText(ctx, navigatieCreatieproces.subtitle, navigatieCreatieproces.position.x - navigatieCreatieproces.dimensions.width + PADDING, navigatieCreatieproces.position.y + (PADDING * 1.5) + navigatieCreatieproces.detailimg.height, COLOR.black, FONT.bold, '18px', 0);
-           creatieprocesLines.forEach(line => {
-            this.drawText(ctx, line, navigatieCreatieproces.position.x - navigatieCreatieproces.dimensions.width + PADDING, navigatieCreatieproces.position.y + (PADDING * 2.5) + navigatieCreatieproces.detailimg.height, COLOR.black, FONT.regular, '14px', offset);
-            offset += 25;
-          });
-          this.drawPointer(ctx, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y, navigatieCreatieproces.position.x -35, navigatieCreatieproces.position.y, navigatieCreatieproces.position.x, navigatieCreatieproces.position.y +35, COLOR.red);
-          break;
+        lines = wrapTextFromCanvasIntoLines(ctx, text, dimensions.width - (PADDING * 10.5));
+
+        this.drawCard(ctx, title, lines, offset, img, image, position, dimensions, options);
       }
 
-
+      
     }else if(parent === 'levensloop'){
 
+    }
+  }
+
+  drawCard(ctx, title, lines, offset, img, image, position, dimensions, options){
+    switch(options.pointedTo){
+      case 'lowerLeft':
+          this.drawBox(ctx, position.x, position.y, dimensions.width, -dimensions.height, COLOR.white);
+          this.drawText(ctx, title, position.x + PADDING, position.y - dimensions.height + (PADDING * 1.5), COLOR.black, FONT.bold, '18px', 0);
+          lines.forEach(line => {
+            this.drawText(ctx, line, position.x + PADDING,position.y - dimensions.height + (PADDING * 2.5), COLOR.black, FONT.regular, '14px', offset);
+            offset += 25;
+          });
+          this.drawImage(ctx, image, position.x, position.y - img.height, img.width, img.height);
+          this.drawPointer(ctx, position.x, position.y -35, position.x, position.y, position.x + 35, position.y, COLOR.red);
+      break;
+
+      case 'topLeft':
+        this.drawBox(ctx, position.x, position.y, dimensions.width, dimensions.height, COLOR.white);
+        this.drawText(ctx, title, position.x + PADDING, position.y+ (PADDING * 1.5) + img.height, COLOR.black, FONT.bold, '18px', 0);
+        lines.forEach(line => {
+          this.drawText(ctx, line, position.x + PADDING,position.y + (PADDING * 2.5) + img.height, COLOR.black, FONT.regular, '14px', offset);
+          offset += 25;
+        });
+        this.drawImage(ctx, image, position.x, position.y, img.width, img.height);
+        this.drawPointer(ctx, position.x, position.y, position.x, position.y +35, position.x +35, position.y, COLOR.red);
+      break;
+
+      case 'topRight':
+       this.drawBox(ctx, position.x, position.y, -dimensions.width, dimensions.height, COLOR.white);
+       this.drawText(ctx, title, position.x - dimensions.width + PADDING, position.y + (PADDING * 1.5) + img.height, COLOR.black, FONT.bold, '18px', 0);
+       lines.forEach(line => {
+        this.drawText(ctx, line, position.x - dimensions.width + PADDING,position.y + (PADDING * 2.5) + img.height, COLOR.black, FONT.regular, '14px', offset);
+        offset += 25;
+      });
+      this.drawImage(ctx, image, position.x - img.width, position.y, img.width, img.height);
+      this.drawPointer(ctx, position.x, position.y, position.x -35, position.y, position.x, position.y +35, COLOR.red);
+      break;
+
+      case 'lowerRight':
+        this.drawBox(ctx, position.x, position.y, -dimensions.width, -dimensions.height, COLOR.white);
+        this.drawText(ctx, title, position.x - dimensions.width + PADDING, position.y - dimensions.height + (PADDING * 1.5), COLOR.black, FONT.bold, '18px', 0);
+        lines.forEach(line => {
+          this.drawText(ctx, line, position.x - dimensions.width + PADDING, position.y - dimensions.height + (PADDING * 2.5), COLOR.black, FONT.regular, '14px', offset);
+          offset += 25;
+        });
+        this.drawImage(ctx, image, position.x -  img.width, position.y - img.height, img.width, img.height);
+        this.drawPointer(ctx, position.x, position.y, position.x -35, position.y, position.x, position.y -35, COLOR.red);
+      break;
+
+      default:
+        console.log('No direction provided in json data.')
+      break;
     }
   }
 
