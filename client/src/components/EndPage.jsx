aaae6de8bf5eb9f7589d5d6747be8ef150389c53
justineVanderplancke <justine.vanderplancke@student.howest.be>
2019-02-11 20:57:25 +0100

    Merge branch 'Development' of https://github.com/JeromVerschoote/van-eyck-interactive-installation into Development

--- client/src/components/EndPage.jsx
+++ client/src/components/EndPage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 
+import handtekening from'../assets/img/endHandtekening.png';
+import zelfportret from'../assets/img/endPage.png';
+
 class EndPage extends Component {
   render() {
     const {title, text} = this.props.data;
@@ -9,8 +12,10 @@ class EndPage extends Component {
     return (
         <section className={type === 'odd' ? 'page page--odd page--end' :  'page page--even page--end'}>
         <div className='content'>
-            <h1>{title}</h1>
-            <p>{text}</p>
+            <h1 className='h1'>{title}</h1>
+            <p className='paragraph'> {text}</p>
+            <img src={zelfportret} alt="Dit is de zelfportret van Van Eyck" width="600" height="400" />
+            <img src={handtekening} alt="Dit is de handtekening van Van Eyck" width="295" height="200" />
         </div>
       </section>
     )
